03bd92f89a15d098c89e44d8585d783aec0c5409
Da <1591820729@qq.com>
2018-08-31 15:49:01 +0800

    123

---
+++ src/org/phw/eop/api/bean/usercustchg/req/ActorInfo.java
@@ -0,0 +1,49 @@
+package org.phw.eop.api.bean.usercustchg.req;
+
+public class ActorInfo {
+    private String actorCertTypeId;
+    private String actorCertNum;
+    private String actorPhone;
+    private String actorName;
+    private String actorAddress;
+
+    public String getActorCertTypeId() {
+        return actorCertTypeId;
+    }
+
+    public void setActorCertTypeId(String actorCertTypeId) {
+        this.actorCertTypeId = actorCertTypeId;
+    }
+
+    public String getActorCertNum() {
+        return actorCertNum;
+    }
+
+    public void setActorCertNum(String actorCertNum) {
+        this.actorCertNum = actorCertNum;
+    }
+
+    public String getActorPhone() {
+        return actorPhone;
+    }
+
+    public void setActorPhone(String actorPhone) {
+        this.actorPhone = actorPhone;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    public String getActorAddress() {
+        return actorAddress;
+    }
+
+    public void setActorAddress(String actorAddress) {
+        this.actorAddress = actorAddress;
+    }
+}
